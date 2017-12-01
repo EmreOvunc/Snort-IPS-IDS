dfe55e2f848b5c9b29c3ba18617b62235c7d9c3b
Emre <EmreOvunc@users.noreply.github.com>
2017-12-01 14:29:31 +0300

    Add files via upload

---
+++ change-rules.py
@@ -0,0 +1,17 @@
+import os,platform
+
+drc_3 = "/etc/snort/rules"
+for dirName, subdirList, fileList in os.walk(drc_3):
+    for fname in fileList:
+        try:
+            if fname.endswith(".rules") and fname.startswith("1community-web-client"):
+                print fname
+                x=fname
+                f1 = open(x, 'r+')
+                f2 = open(x, 'r+')
+                for line in f1:
+                    f2.write(line.replace('flowbits:noreject;', ''))
+                f1.close()
+                f2.close()
+        except:
+            pass
